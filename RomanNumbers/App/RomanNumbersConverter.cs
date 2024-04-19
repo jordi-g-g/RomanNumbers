819158d3ce1f488806886cd08968a007b67811f6
JordiGG <jordi.grados@codurance.com>
2024-04-19 14:40:39 +0200

    adding more roman numbers

--- RomanNumbers/App/RomanNumbersConverter.cs
+++ RomanNumbers/App/RomanNumbersConverter.cs
@@ -6,6 +6,27 @@ public class RomanNumbersConverter
     {
         var output = "";
 
+        if (number == 10 - 1)
+        {
+            output += "XI";
+            number -= 10 - 1;
+        }
+        while (number >= 10)
+        {
+            if (number == 10)
+            {
+                output += "X";
+                number -= 10;
+                break;
+            }
+            output += "I";
+            number -= 1;
+        }
+        if (number == 5 - 1)
+        {
+            output += "VI";
+            number -= 5 - 1;
+        }
         while (number >= 5)
         {
             if (number == 5)
--- RomanNumbers/RomanNumbersConverterShould.cs
+++ RomanNumbers/RomanNumbersConverterShould.cs
@@ -14,10 +14,14 @@ namespace RomanNumbers
             Assert.That("I", Is.EqualTo(romanNumbersConverter.Convert(1)));
             Assert.That("II", Is.EqualTo(romanNumbersConverter.Convert(2)));
             Assert.That("III", Is.EqualTo(romanNumbersConverter.Convert(3)));
+            Assert.That("IV", Is.EqualTo(romanNumbersConverter.Convert(4)));
             Assert.That("V", Is.EqualTo(romanNumbersConverter.Convert(5)));
             Assert.That("VI", Is.EqualTo(romanNumbersConverter.Convert(6)));
             Assert.That("VII", Is.EqualTo(romanNumbersConverter.Convert(7)));
             Assert.That("VIII", Is.EqualTo(romanNumbersConverter.Convert(8)));
+            Assert.That("IX", Is.EqualTo(romanNumbersConverter.Convert(9)));
+            Assert.That("X", Is.EqualTo(romanNumbersConverter.Convert(10)));
+            Assert.That("XI", Is.EqualTo(romanNumbersConverter.Convert(11)));
         }
     }
 }
